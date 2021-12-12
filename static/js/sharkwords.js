193d26aa283355ec8a6583a1e913b274eb7908e2
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-12 01:30:13 +0000

    Completed sharkwords.js

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -15,6 +15,7 @@ const WORDS = [
 ];
 
 let numWrong = 0;
+let numCorrect = 0;
 
 // Loop over the chars in `word` and create divs.
 //
@@ -43,15 +44,42 @@ const disableLetterButton = buttonEl => {
   buttonEl.disabled = true;
 };
 
+const disableAllLetterButtons = () => {
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.disabled = true;
+  }
+  
+}
+
+
 // Return `true` if `letter` is in the word.
 //
 const isLetterInWord = letter => document.querySelector(`div.${letter}`) !== null;
 
 // Called when `letter` is in word. Update contents of divs with `letter`.
 //
-const handleCorrectGuess = letter => {
-  // Replace this with your code
+const handleCorrectGuess = (letter, word) => {
+  const letterDivs = document.querySelectorAll(`div.${letter}`);
+  
+  for (const letterDiv of letterDivs) {
+      letterDiv.innerHTML = letter;
+      numCorrect += 1;
+  }
+
+  // if num of div.letters == len(word)
+  if (word.length === numCorrect) {
+    disableAllLetterButtons();
+    document.querySelector('#win').style.display = '';
+  }
 };
+
+
+// <a id="win" style="display: none;">
+// Congratulations! 🥳 You won!
+// </a>
+
 
 //
 // Called when `letter` is not in word.
@@ -63,12 +91,22 @@ const handleCorrectGuess = letter => {
 const handleWrongGuess = () => {
   numWrong += 1;
   // Replace this with your code
-};
+
+  if (numWrong < 5) {
+    const sharkPhoto = document.querySelector('img');
+    sharkPhoto.setAttribute('src',`/static/images/guess${numWrong}.png`);
+  } 
+    else {
+    disableAllLetterButtons()
+    document.querySelector('#play-again').style.display = ''; 
+  };
+}
 
 //  Reset game state. Called before restarting the game.
 const resetGame = () => {
   window.location = '/sharkwords';
 };
+
 
 // This is like if __name__ == '__main__' in Python
 //
@@ -79,9 +117,31 @@ const resetGame = () => {
   createDivsForChars(word);
   generateLetterButtons();
 
+  const buttons = document.querySelectorAll('button');
   // add an event handler to handle clicking on a letter
-  // YOUR CODE HERE
+
+  for (const button of buttons) {
+    button.addEventListener('click', (evt) => {
+      const clickedBtn = evt.target;
+
+      // you should disable the button so the letter can't be clicked again
+      disableLetterButton(clickedBtn);
+  
+      const letter = clickedBtn.textContent;
+
+      // you should then check if the currently clicked letter is in the word
+      if (isLetterInWord(letter)) {
+      // if it is, call `handleCorrectGuess`
+        handleCorrectGuess(letter, word); 
+      } else {
+        // if it is not, call `handleWrongGuess`
+        handleWrongGuess(letter);
+      }
+    })
+  }
 
   // add an event handler to handle clicking on the Play Again button
-  // YOUR CODE HERE
+  document.querySelector('#play-again').addEventListener('click', resetGame);
+  document.querySelector('#win').addEventListener('click', resetGame);
+
 })();
